085c44ce4c847be4336d72e98e7ca2a288158ab2
ttcbli <15935498245@163.com>
2019-09-02 10:25:12 +0800

    安装

---
+++ janebookweb/src/views/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <mt-tabbar v-model="selected">
+            <mt-tab-item id="tab1">
+                <img slot="icon" src="">
+                tab1
+            </mt-tab-item>
+            <mt-tab-item id="tab2">
+                <img slot="icon" src="">
+                tab2
+            </mt-tab-item>
+            <mt-tab-item id="tab3">
+                <img slot="icon" src="">
+                tab3
+            </mt-tab-item>
+            <mt-tab-item id="tab4">
+                <img slot="icon" src="">
+                tab4
+            </mt-tab-item>
+        </mt-tabbar>
+    </div>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            selected:'事件'
+        }
+    },
+    methods: {
+        openPicker() {
+            this.$refs.picker.open();
+        }
+    },
+    components:{
+    }
+}
+</script>
