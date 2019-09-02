208e5b014ef4846e76c7bb5a694415c83e3054f6
ttcbli <15935498245@163.com>
2019-09-02 19:04:20 +0800

    userlogin

--- janebookweb/src/router.js
+++ janebookweb/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Index from './views/index.vue'
+import Userlogin from './views/Userlogin.vue'
 
 Vue.use(Router)
 
@@ -13,9 +13,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/Index',
-      name: 'Index',
-      component: Index
+      path: '/Userlogin',
+      name: 'Userlogin',
+      component: Userlogin
     },
     {
       path: '/about',
---
+++ janebookweb/src/views/Userlogin.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="aaa">
+        <div class="title">
+            <img src="../assets/login.png" alt="">
+            <h2>asdasd</h2>
+        </div>
+        <mt-field label="手机号" placeholder="请输入手机号" v-model="userphone"></mt-field>
+        <mt-field label="密码" placeholder="请输入密码" v-model="userpwd" ></mt-field>
+        <div>
+            <input type="checkbox" id="bool"><label for="bool">asdasdas</label>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            userphone:'',
+            userpwd:'',
+        }
+    },
+}
+</script>
+<style scoped>
+    .aaa{
+        position: absolute;
+        top:20%;
+        left:50%;
+        margin-left:-160px;
+    }
+    div{
+        text-align: center;
+    }
+    .title{
+        display: flex;
+        align-items: baseline;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
